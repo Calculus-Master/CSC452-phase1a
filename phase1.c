cbb2d1166108f5f8a9f2c4297660f37e319211da
Saptarshi Mallick <sapmallick@gmail.com>
2024-09-22 16:04:10 -0700

    Created basic Process struct and process table

--- phase1.c
+++ phase1.c
@@ -1,8 +1,20 @@
 #include <phase1.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct Process {
+    int pid;
+    int priority;
+    int status;
+    char name[MAXNAME];
+    USLOSS_Context context;
+} Process;
+
+Process process_table[MAXPROC];
 
 void phase1_init(void)
 {
-
+    memset(process_table, 0, sizeof(process_table));
 }
 
 int spork(char *name, int(*func)(void *), void *arg, int stacksize, int priority)
